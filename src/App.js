235371a1367281b9552a951e270cb2baba65cb9b
Saharat Shoedsuk <6310110501@psu.ac.th>
2022-08-05 01:18:43 +0700

    add css class name Card

--- src/App.js
+++ src/App.js
@@ -10,4 +10,15 @@ function App() {
   ); 
 } 
 
+const word = "Hello";
+function App(){
+  return (
+    <div>
+      {
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
+    </div>
+  );
+}
+
 export default App;
