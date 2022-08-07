9415e77909d8453f21369a8d1be0ba92d85f6084
Saharat Shoedsuk <6310110501@psu.ac.th>
2022-08-07 19:26:11 +0700

    button refresh

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import WordCard from './WordCard';
 
 
 const word = 'Hello';
+const buttonrefresh = () => {
+  window.location.reload();
+}
 
 function App(){
   return (
     <div className = 'box'>
         <WordCard value={word}/>
+        <button className = 'button' onClick={buttonrefresh}>new game</button>
     </div>
   );
 }
