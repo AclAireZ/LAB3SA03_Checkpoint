ae902355f5630f0c8c109a5a0a57f9d4538dd035
Saharat Shoedsuk <6310110501@psu.ac.th>
2022-08-07 19:17:02 +0700

    frame

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import  CharacterCard from './CharacterCard';
 import './App.css';
 import WordCard from './WordCard';
 
+
 const word = 'Hello';
+
 function App(){
   return (
-    <div>
+    <div className = 'box'>
         <WordCard value={word}/>
     </div>
   );
